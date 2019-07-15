230f13c15d7c4e7137a4b0927e00ca48d0d428c7
张必超 <zhangbichao@zhangbihaodeAir.lan>
2019-07-16 03:01:35 +0800

    Add the map file

---
+++ Algor/code/map/y.cpp
@@ -0,0 +1,37 @@
+#include "iostream"
+#include <string>
+#include <map>
+
+using namespace std;
+
+typedef struct zbh {
+  int index;
+  zbh(int t) {
+    index = t;
+  }
+  friend bool operator < (const zbh z1, const zbh z2) {
+    return z1.index < z2.index;
+  }
+} zbh;
+
+int main() {
+  std::string s = "hello";
+  std::map<string, int> testmap1;
+  testmap1.insert(pair<string,int>(s,100));
+  for (auto &item : testmap1) {
+    cout << "test: " << item.first << endl;
+  }
+  zbh z(12);
+  std::map<zbh,string> testmap2;
+  testmap2.insert(pair<zbh,string>(z,s));
+  for (auto &item : testmap2) {
+    cout << "test: " << endl;
+  }
+
+  std::map<zbh*, string> testmap3;
+  testmap3.insert(pair<zbh*, string>(&z,s));
+  for (auto &item : testmap3) {
+    cout << "test: " << endl;
+  }
+
+}
