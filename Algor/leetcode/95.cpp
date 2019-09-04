b39871fac84ee00a8ec53c3178b367139a394de1
张必超 <zhangbichao@zhangbihaodeAir.lan>
2019-09-05 01:38:59 +0800

    new 95

---
+++ Algor/leetcode/95.cpp
@@ -0,0 +1,91 @@
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
+class Solution {
+public:
+bool flag[1000] = {false};
+vector<TreeNode*> generateTrees(int n) {
+vector<TreeNode*> tree;
+vector<std::string> > res;
+std::string cur;
+for (int i = 1;i <= n ;i++) {
+    cur = std::to_string(i);
+    flag[i] = true;
+    dfs(res, cur, n-1, n);
+    flag[i] = false;
+    cur.clear();
+}
+
+buildTree(res, tree);
+return tree;
+}
+
+  void dfs(vector<std::string> &res, std::string cur, int k, int n) {
+    if (k == 0) {
+      res.push_back(cur);
+      return;
+    }
+    for (int i = 1;i <= n; i++) {
+      if (flag[i] == true)
+	continue;
+      flag[i] = true;
+      dfs(res, cur + std::to_string(i), k-1,n);
+      flag[i] = false;
+    }
+}
+
+void buildTree(vector<std::string > &res, vector<TreeNode*> &tree) {
+for(string &item : res) {
+  TreeNode *root = new TreeNode(std::stoi(s[0])); 
+    for (int i = 1; i < item.length(); i++) {
+      TreeNode *node = new TreeNode(std::stoi(item[i])); 
+    InsertNode(root, node);
+    }
+    tree.push_back(root);
+}
+
+  void InsertNode(TreeNode *root, TreeNode *node) {
+    if (root == NULL)
+      return;
+    if (root->val < node->val) {
+      if (root->right == NULL)
+	root->right = node;
+      else
+	InsertNode(root->right, node);
+    } else if (root->val > node->val) {
+      if (root->left == NULL)
+	root->left = node;
+      else
+	InsertNode(root->left, node);
+    }
+  }
+
+vector<TreeNode*>::iterator it;
+std::string res_str;
+map<std::string, vector<TreeNode*>::iterator> map_v;
+for (it = tree.begin(); it != tree.end();) {
+    visitTree(*it, res_str);     
+    if (map_v.find(res_str) != map_v.end()) {
+    it = tree.erase(it);	
+    } else {
+    map_v[res_str] = it;
+    it++;
+    }
+    res_str.clear();
+}
+}
+
+void visitTree(TreeNode *tree, std::string &res) {
+  if (tree) {
+   res += std::to_string(tree->val);
+   visitTree(tree->left);
+   visitTree(tree->right);
+}
+};
+
